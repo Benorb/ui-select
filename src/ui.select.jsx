4bbc6f2f498d153e2d7e3a760d8c6c935e9128b5
Benorb <benorb@welldonw-software.com>
2018-04-14 22:28:19 +0300

    Select

--- src/ui.select.jsx
+++ src/ui.select.jsx
@@ -3,15 +3,24 @@ import './ui.select.css';
 
 class UISelect extends Component {
 
-  state = {
+  constructor(props){
+    super(props);
+
+    this.refsArr = [];
+
+    this.state = {
       dropdownOpen: true,
-  };
-  
-  componentWillMount(){
-    document.addEventListener("keydown", this.handleDownKey, false);
+      focusedIndex: null,
+      selectedId: null,
+      mouseControl: false
+    };
   }
   
-  componentWillUnmount(){
+  componentWillMount() {
+    document.addEventListener("keydown", this.handleDownKey, false);
+  }
+
+  componentWillUnmount() {
     document.removeEventListener("keydown", this.handleDownKey, false);
   }
 
@@ -22,15 +31,59 @@ class UISelect extends Component {
     if(e.keyCode === 40 && !dropdownOpen){
       this.setState({ dropdownOpen: true })
     }
+
+    if(dropdownOpen){
+      this.handleKeyboardNavigation(e)
+    }
+  }
+
+  handleKeyboardNavigation = (e) => {
+    const { focusedIndex } = this.state;
+    const { options } = this.props;
+
+    switch(e.keyCode) {
+      case 38: { // up arrow
+        const newIndex = focusedIndex - 1;
+        focusedIndex > 1 && this.setState({focusedIndex: newIndex}, () => {
+          this.scrollToIndex(newIndex);
+        });
+        break;
+      }
+      case 40: { // down arrow
+        const newIndex = focusedIndex + 1;
+        focusedIndex <  options.length - 1 && this.setState({focusedIndex: newIndex}, () => {
+          this.scrollToIndex(newIndex);
+        });
+        break;
+      }
+      case 13: { // enter
+        console.log(options[focusedIndex]);
+        break;
+      }
+      default:
+        return
+    }
+  }
+
+  scrollToIndex = (index) => {
+    this.refsArr[index].scrollIntoView({ block: "center", inline: "nearest" });
   }
 
   onSelectItem = (item, i) => {
-    console.log(item)
-    this.setState({ dropdownOpen: false });
+    console.log(item);
+    this.setState({
+      dropdownOpen: false,
+      selectedId: item.id,
+      focusedIndex: i
+    });
   }
 
   toggleSelect = () => {
     const { dropdownOpen } = this.state;
+    if(dropdownOpen) {
+      // If the dropdown is currently opened, so its about to be closed.
+      this.clearFocusedIndex();
+    }
     this.setState({dropdownOpen: !dropdownOpen});
   }
 
@@ -41,47 +94,63 @@ class UISelect extends Component {
     dropdownOpen && this.setState({dropdownOpen: false});
   }
 
-  onInputChange = (e) => {
-    console.log('e', e)
+  onMouseOver = (e,i) => {
+    this.setState({focusedIndex: i});
+  }
+
+  onMouseOut = () => {
+    this.clearFocusedIndex();
+  }
+
+  clearFocusedIndex = () => {
+    this.setState({focusedIndex: null});
+  }
+
+  setRef = (ref, i) =>{
+    this.refsArr[i] = ref;
+  }
+
+  click = () => {
+    console.log(this.refsArr);
+    this.refsArr[4].scrollIntoView();
   }
 
   render() {
-    const { dropdownOpen } = this.state;
+    const { dropdownOpen, focusedIndex, selectedId  } = this.state;
     const { options } = this.props;
     const arrowDirection = dropdownOpen ? 'arrow-up' : 'arrow-down';
 
     return (
       <div
         className="container"
-        tabIndex="0"
+        tabIndex={0}
         onBlur={ this.onBlur }
       >
         <div className="select" onClick={this.toggleSelect}>
-          <div className="text">
-            {options.find((item) => item.id === null).name}
+          <div className="text" id="hash">
+            {options.find((item) => item.id === selectedId).name}
             {/*If i could assume that the placeholder will always be the first value,
                so i would do {options[0].name}  */}
           </div>
 
-          <div className="text">
-              <div className={arrowDirection}/>
-          </div>
+          <div className={arrowDirection}/>
+
         </div>
 
         {
           dropdownOpen ?
             <div>
-              <input
-                ref={(input) => { this.inputRef = input }}
-                className="hidden-input"
-                // onKeyPress={this.onInputChange}
-                // type="hidden"
-              />
               <div className="triangle-up"/>
               <div className="dropdown">
+                {/*<input type="hidden"/>*/}
                 {
                   options.map((item, i) =>
-                  <div key={item.id} className="listRow">
+                  <div
+                    key={item.id}
+                    onMouseOver={(e) => this.onMouseOver(e,i)}
+                    onMouseOut={this.onMouseOut}
+                    ref={(ref) => this.setRef(ref, i)}
+                    className={`listRow ${focusedIndex === i ? 'active' : ''} ${item.id === null ? 'disabled' : ''}`}>
                     <div
                       className={`dropdown-text`}
                       onClick={() => this.onSelectItem(item, i)}>
